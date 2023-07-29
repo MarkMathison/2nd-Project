0bdb212947b8ae224552a38b25002ecef3c2b28c
Mark <markmathison12783@gmail.com>
2023-07-28 19:43:38 -0500

    conversation complete

--- ai-saas/components/sidebar.tsx
+++ ai-saas/components/sidebar.tsx
@@ -3,6 +3,8 @@ import Link from "next/link"
 import Image from "next/image"
 import { Montserrat } from "next/font/google"
 
+import { usePathname } from "next/navigation";
+
 import { cn } from "@/lib/utils"
 import { Code, ImageIcon, LayoutDashboard, MessageSquare, Music, Settings, VideoIcon } from "lucide-react";
 
@@ -48,11 +50,13 @@ const routes=[
     {
         label: 'Settings',
         icon:  Settings,
-        href: "/settings",
+        href: "/settings"
     },
 ]
 
 const Sidebar= () =>{
+const pathname=usePathname()
+
     return(
         <div className="space-y py-4 flex flex-col h-full bg-[#111827] text-white">
             <div className="px-3 py-2 flex-1">
@@ -73,8 +77,9 @@ const Sidebar= () =>{
                         <Link
                             href={route.href}
                             key={route.href}
-                            className="text-sm group flex p-3 w-full justify-start font-medium cursor-pointer
-                            hover:text-white hover:bg-white/10 rounded-lg transition"
+                            className={cn("text-sm group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-white hover:bg-white/10 rounded-lg transition",
+                                pathname===route.href ? "text-white bg-white/10" : "text-zinc-400"
+                            )}
                             >
                                 <div className="flex items-center flex-1">
                                     <route.icon className={cn("h-5 w-5 mr-3", route.color)}/>
