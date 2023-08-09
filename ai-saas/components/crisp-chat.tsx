a6e57405bafafdd2f876237bed0129ae85c1b747
Mark <markmathison12783@gmail.com>
2023-08-09 16:48:48 -0500

    i think it work

--- ai-saas/components/crisp-chat.tsx
+++ ai-saas/components/crisp-chat.tsx
@@ -5,7 +5,7 @@ import { Crisp } from "crisp-sdk-web";
 
 export const CrispChat = () => {
   useEffect(() => {
-    Crisp.configure("8254f25a-eca0-421e-b4de-2bf655c03887");
+    Crisp.configure("206b5249-cb0a-4277-97a4-87cba679e61d");
   }, []);
 
   return null;
